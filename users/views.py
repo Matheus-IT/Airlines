5f9fc637418cc0e5466845d559c24b831917694b
Matheus <matheus1250.mc@gmail.com>
2020-09-09 16:29:58 -0300

    theres's a bug here

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('users:login'))
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST('username')
+        password = request.POST('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('users:index'))
+        else:
+            return render(request, 'users/login.html', {
+                'message': 'Invalid credentials.'
+            })
+
+
+def logout(request):
+    pass
